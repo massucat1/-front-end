bfa5b1a7e81044d712941ee860882ded7c207a4d
masucat1 <massucatocarlos@gail.com>
2024-03-04 11:51:26 -0300

    aula 10 HTML

---
+++ aula 09/script/imagem.js
@@ -0,0 +1,27 @@
+let images = [,]/*criando um alista vazia para guardar as imagens*/
+
+function carregarImagem(){
+    const galeria = document.getElementById("galeria")
+    galeria.innerHTML = '';
+
+    images.forEach(imgURL=>{
+        const img = document.createElement("img");
+        img.src = imgURL;
+        galeria.appendChild(img)
+    })
+}
+
+document.getElementById('arquivos').addEventListener('change',function(event){
+   const arquivo = event.target.files[0];
+   const ler = new FileReader();
+
+   ler.onload = function(e){
+    const imgURL = e.target.result;
+    images.push(imgURL);
+    carregarImagem();
+   };
+
+   ler.readAsDataURL(arquivo)
+
+
+})
